af1c2ae39c508e9fd83c252fb6846d88f349fd2f
javan7 <orucovcavanshir@gmail.com>
2022-02-18 12:40:12 +0300

    second slider

--- dist/splite.dev.js
+++ dist/splite.dev.js
@@ -8,3 +8,13 @@ new Splide(".splide", {
   pagination: false,
   arrows: false
 }).mount();
+new Splide(".splide2", {
+  type: "loop",
+  speed: number = 1500,
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+  perPage: 4,
+  perMove: 1
+}).mount();
--- splite.js
+++ splite.js
@@ -6,3 +6,14 @@ new Splide(".splide", {
   pagination: false,
   arrows: false,
 }).mount();
+
+new Splide(".splide2", {
+  type: "loop",
+  speed: (number = 1500),
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+  perPage: 4,
+  perMove: 1,
+}).mount();
