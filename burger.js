830827bedc1c9cf0cb5483a6d5df5e136c1b4e91
javan7 <orucovcavanshir@gmail.com>
2022-04-22 11:46:50 +0300

    burger-x

--- burger.js
+++ burger.js
@@ -1,9 +1,11 @@
 let menuBtn = document.querySelector(".menu-burger");
 let nav = document.querySelector(".nav");
 let rotate = document.querySelector(".header-info-rotate");
+let line = document.querySelector(".burger-line");
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("action");
   nav.classList.toggle("action");
   rotate.classList.toggle("action");
+  line.classList.toggle("action");
 });
--- dist/burger.dev.js
+++ dist/burger.dev.js
@@ -3,8 +3,10 @@
 var menuBtn = document.querySelector(".menu-burger");
 var nav = document.querySelector(".nav");
 var rotate = document.querySelector(".header-info-rotate");
+var line = document.querySelector(".burger-line");
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("action");
   nav.classList.toggle("action");
   rotate.classList.toggle("action");
+  line.classList.toggle("action");
 });
